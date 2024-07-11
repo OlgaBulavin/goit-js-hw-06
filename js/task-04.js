a1a53f689779612287003b29ba89e40a6295135a
Olga Bulavin <olga.bulavin89@gmail.com>
2024-07-12 00:02:18 +0200

    hw-js

--- js/task-04.js
+++ js/task-04.js
@@ -7,7 +7,7 @@ const incrementBtn = document.querySelector("button[data-action='increment']");
 
 decrementBtn.addEventListener("click", () => {
     if (counterValue <= 0) {
-        counterValue === 0;
+        counterValue = 0;
       } ;
     counterValue -= 1;
     counter.textContent = counterValue;
